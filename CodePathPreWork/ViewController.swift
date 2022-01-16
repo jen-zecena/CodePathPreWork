38745d59b867647ecabee5c5d305228ebdccfaee
jen-zecena <jzecenay@gmail.com>
2022-01-15 19:25:50 -0800

    Initial Commit

---
+++ CodePathPreWork/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CodePathPreWork
+//
+//  Created by Jennifer Zecena on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+    }
+    
+}
+
