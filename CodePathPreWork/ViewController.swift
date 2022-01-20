f4012216941ebd64f8934142b74afeda5258f30a
jen-zecena <jzecenay@gmail.com>
2022-01-19 22:26:04 -0800

    Creating a new settings view and adding more functionality to the main page

--- CodePathPreWork/ViewController.swift
+++ CodePathPreWork/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //sets the title of the navigation bar
+        self.title = "Tip Calculator"
     }
 
     @IBAction func calculateTip(_ sender: Any) {
---
+++ SettingsViewControlelrViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewControlelrViewController.swift
+//  CodePathPreWork
+//
+//  Created by Jennifer Zecena on 1/19/22.
+//
+
+import UIKit
+
+class SettingsViewControlelrViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
